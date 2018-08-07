3603eb8f716d1eff12a9f9f290877efe5f5ecd73
Bob van der Linden <bobvanderlinden@gmail.com>
2018-08-07 19:03:29 +0200

    add coin command as alternative to beep

--- home.nix
+++ home.nix
@@ -1,4 +1,13 @@
 { pkgs, ... }:
+let
+  coinSound = pkgs.fetchurl {
+    url = "https://themushroomkingdom.net/sounds/wav/smw/smw_coin.wav";
+    sha256 = "18c7dfhkaz9ybp3m52n1is9nmmkq18b1i82g6vgzy7cbr2y07h93";
+  };
+  coin = pkgs.writeShellScriptBin "coin" ''
+     ${pkgs.sox}/bin/play --no-show-progress ${coinSound}
+  '';
+in
 {
   home.packages = with pkgs; [
     htop
@@ -30,6 +39,7 @@
     lxappearance-gtk3
     baobab
     gitAndTools.hub
+    coin
   ];
   home.file.".config/termite/config".text = ''
     [options]

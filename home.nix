a7ae8c92f03b552d94939ab1c03593466a3b8067
Bob van der Linden <bobvanderlinden@gmail.com>
2018-08-26 17:35:10 +0200

    add 'git pp'

--- home.nix
+++ home.nix
@@ -378,6 +378,7 @@ in
 	    pc = "commit --patch";
 	    rf = "reflog";
 	    l = "log --graph --pretty='%Cred%h%Creset - %C(bold blue)<%an>%Creset %s%C(yellow)%d%Creset %Cgreen(%cr)' --abbrev-commit --date=relative";
+      pp = "!git push --set-upstream origin $(git rev-parse --abbrev-ref HEAD)";
       recent-branches = "branch --sort=-committerdate";
     };
     extraConfig = {
